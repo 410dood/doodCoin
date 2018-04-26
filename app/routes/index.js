f8622a192ac7a3f3c07ed7d1aa4155eb0c5e1f32
Bill Doody <wdoody@yahoo.com>
2018-04-26 14:38:03 -0600

    fixed some stuff

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,19 @@
+var mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
+
+if (process.env.NODE_ENV == 'production') {
+  console.log('connecting to... ' + process.env.NODE_ENV)
+  console.log('also connecting to mlab ' + process.env.MLAB_URL)
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  console.log('this is the local server ')
+  mongoose.connect('mongodb://localhost/doodcoin');
+}
+
+module.exports.User = require('./users');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
