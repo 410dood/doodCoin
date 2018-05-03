1d18e60045d4ae748525609e171cd6cf2077605d
Bill Doody <wdoody@yahoo.com>
2018-05-03 13:58:57 -0600

    fix connectdb routes

--- app/config/database.js
+++ app/config/database.js
@@ -1,6 +1,13 @@
+var mongoose = require("mongoose");
+if (process.env.NODE_ENV == "production") {
+    console.log("connecting to... " + process.env.NODE_ENV)
+    console.log("also connecting to mlab  " + process.env.MLAB_URL)
+    mongoose.connect(process.env.MLAB_URL)
+} else {
+    console.log("this is the local server ")
+    mongoose.connect("mongodb://localhost/doodcoin");
+}
 
-module.exports = {
 
-    'url': 'mongodb://localhost/doodCoin'
-
-};
+module.exports.Place = require("./auth");
+module.exports.User = require("./passport");
